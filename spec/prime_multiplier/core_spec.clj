3c4417ea22fd409f87396e1863be227993b1dcd9
Chris Devine <ChrisDevo@users.noreply.github.com>
2015-08-05 17:15:02 +0100

    Adds new -main function
      * no-arg version calls pprint-product-table with fixed args
      * multiple arg version allows custom header and numbers for table

--- spec/prime_multiplier/core_spec.clj
+++ spec/prime_multiplier/core_spec.clj
@@ -1,15 +1,28 @@
 (ns prime-multiplier.core-spec
   (:require [speclj.core :refer :all]
-            [prime-multiplier.core :refer :all]))
+            [prime-multiplier.core :refer :all]
+            [prime-multiplier.multiplier :refer [pprint-product-table]]
+            [prime-multiplier.primes :refer [first-ten-primes]]))
 
 (describe "main"
-          (it "returns nil when no args given"
-              (should= nil (-main)))
-          (it "returns [\"arg1\"] when given \"arg1\""
-              (should= (list "arg1") (-main "arg1")))
-          (it "returns [\"arg1\" \"arg2\"] when given \"arg1\" \"arg2\""
-              (should= (list "arg1" "arg2") (-main "arg1" "arg2")))
-          (it "returns [arg1 arg2] when given arg1"
-              (should= (list 'arg1 'arg2) (-main 'arg1 'arg2))))
+  (it "should call pprint-product-table with \"prime\" and first-ten-primes when no args given"
+    (should-invoke pprint-product-table {:with ["prime" first-ten-primes]} (-main)))
+
+  (it "throws an AssertionError if the first arg isn't a string"
+    (should-throw AssertionError (-main :k))
+    (should-throw AssertionError (-main 1)))
+
+  (it "throws an AssertionError if the rest of the args are missing"
+    (should-throw AssertionError (-main "header")))
+
+  (it "throws an AssertionError if any of the other args are not numbers"
+    (should-throw AssertionError (-main "string" :k))
+    (should-throw AssertionError (-main "string" 'dummy_symbol)))
+
+  (it "should not thorw an AssertionError if any of the other args can be parsed as a number"
+    (should-not-throw (-main "string" "1" 2 3))
+    (should-not-throw (-main "string" 1 "2" 3))
+    (should-not-throw (-main "string" 1 2 "3"))
+    (should-not-throw (-main "string" 1 2 3))))
 
 (run-specs)
--- src/prime_multiplier/core.clj
+++ src/prime_multiplier/core.clj
@@ -1,7 +1,22 @@
 (ns prime-multiplier.core
+  (:require [prime-multiplier.multiplier :refer [pprint-product-table]]
+            [prime-multiplier.primes :refer [first-ten-primes]])
   [:gen-class])
 
-(defn -main [& args]
-  args)
+(defn -main
+  ([]
+   (pprint-product-table "prime" first-ten-primes))
+  ([& args]
+   {:pre [(string? (first args))
+          (not-empty (rest args))
+          (every? #(or (string? %) (number? %)) (rest args))
+          (every? number? (map read-string (filter string? (rest args))))]}
+   (let [header (first args)
+         numbers (rest args)
+         parsed-numbers (map (fn [i]
+                               (if (number? i)
+                                 i
+                                 (read-string i))) numbers)]
+     (pprint-product-table header parsed-numbers))))
 
 
